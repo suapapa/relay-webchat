78ca8fb1d2ea2fd02a0e77f4cb23301e2f540290
Homin Lee <ff4500@gmail.com>
2025-05-05 11:45:07 +0900

    fix: dockerfile to relay.dockerfile

--- push2vultr.sh
+++ push2vultr.sh
@@ -2,6 +2,6 @@
 set -e
 CR=icn.vultrcr.com/homincr1
 IMAGE_TAG=$CR/webchat-relay:latest 
-docker buildx build --platform linux/amd64 -t $IMAGE_TAG .
+docker buildx build --platform linux/amd64 -f relay.dockerfile -t $IMAGE_TAG .
 docker push $IMAGE_TAG
 k rollout restart deployment webchat-relay
